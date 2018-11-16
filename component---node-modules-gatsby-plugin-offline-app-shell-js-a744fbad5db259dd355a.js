591d1b1639ddf0d89f282ef8f825df57dde85255
simon james <simonjames@simons-mbp.home>
2018-11-16 13:42:51 +0000

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-a744fbad5db259dd355a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{142:function(e,t,n){"use strict";var u=n(5);t.__esModule=!0,t.default=void 0;var o=u(n(7)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-a744fbad5db259dd355a.js.map
